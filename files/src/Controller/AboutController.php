757ec6baa68eb4dd8fd19d93e1d3d48223938077
federico-castellani <fedecast.16@gmail.com>
2025-03-13 10:34:16 +0100

    Dependecies

--- files/src/Controller/AboutController.php
+++ files/src/Controller/AboutController.php
@@ -13,9 +13,10 @@ class AboutController extends AbstractController
     #[Route('/about/{subject}', name: 'about')]
     public function index(?string $subject = null): Response
     {
-        if($subject !== null) {
+        if (null !== $subject) {
             return new Response("<html><body>Ciao $subject</body></html>");
         }
+
         return new Response('<html><body>About</body></html>');
     }
 }
