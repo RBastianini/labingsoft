c8c44f8c3fad3fbd689abfa0c04bcf7dbe5f8f4e
Riccardo Bastianini <RBastianini@users.noreply.github.com>
2025-05-16 22:36:19 +0200

    Fine lezione 8

--- files/tests/Unit/Validator/BothPropertiesPresentValidatorTest.php
+++ files/tests/Unit/Validator/BothPropertiesPresentValidatorTest.php
@@ -4,22 +4,87 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Validator;
 
+use App\Validator\BothPropertiesPresent;
+use App\Validator\BothPropertiesPresentValidator;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Exception\InvalidArgumentException;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
 
 class BothPropertiesPresentValidatorTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function it_checks_that_property1_is_readable(): void
+    use MockeryPHPUnitIntegration;
+
+    private BothPropertiesPresentValidator $SUT;
+
+    private PropertyAccessorInterface&MockInterface $propertyAccessor;
+
+    private ExecutionContextInterface&MockInterface $context;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+        $this->propertyAccessor = \Mockery::mock(PropertyAccessorInterface::class);
+        $this->context = \Mockery::spy(ExecutionContextInterface::class);
+        $this->SUT = new BothPropertiesPresentValidator($this->propertyAccessor);
+        $this->SUT->initialize($this->context);
     }
 
     /**
      * @test
      */
-    public function it_checks_that_property2_is_readable(): void
+    public function it_throws_an_exception_if_property1_is_not_readable(): void
     {
+        $valueToValidate = new \stdClass();
+        $property1 = 'someProperty';
+        $property2 = 'someOtherProperty';
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Property {$property1} is not accessible on passed object.");
+
+        $this->propertyAccessor->expects('isReadable')
+            ->with($valueToValidate, $property1)
+            ->andReturn(false);
+
+        $this->SUT->validate(
+            $valueToValidate,
+            new BothPropertiesPresent(
+                property1: $property1,
+                property2: $property2
+            )
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_an_exception_if_property2_is_not_readable(): void
+    {
+        $valueToValidate = new \stdClass();
+        $property1 = 'someProperty';
+        $property2 = 'someOtherProperty';
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Property {$property2} is not accessible on passed object.");
+
+        $this->propertyAccessor->expects('isReadable')
+            ->with($valueToValidate, $property1)
+            ->andReturn(true);
+
+        $this->propertyAccessor->expects('isReadable')
+            ->with($valueToValidate, $property2)
+            ->andReturn(false);
+
+        $this->SUT->validate(
+            $valueToValidate,
+            new BothPropertiesPresent(
+                property1: $property1,
+                property2: $property2
+            )
+        );
     }
 
     /**
@@ -27,6 +92,35 @@ class BothPropertiesPresentValidatorTest extends TestCase
      */
     public function it_passes_validation_if_both_values_are_null(): void
     {
+        $valueToValidate = new \stdClass();
+        $property1 = 'someProperty';
+        $property2 = 'someOtherProperty';
+
+        $this->propertyAccessor->expects('isReadable')
+            ->with($valueToValidate, $property1)
+            ->andReturn(true);
+
+        $this->propertyAccessor->expects('isReadable')
+            ->with($valueToValidate, $property2)
+            ->andReturn(true);
+
+        $this->propertyAccessor->expects('getValue')
+            ->with($valueToValidate, $property1)
+            ->andReturn(null);
+
+        $this->propertyAccessor->expects('getValue')
+            ->with($valueToValidate, $property2)
+            ->andReturn(null);
+
+        $this->SUT->validate(
+            $valueToValidate,
+            new BothPropertiesPresent(
+                property1: $property1,
+                property2: $property2
+            )
+        );
+
+        $this->context->shouldNotHaveReceived('buildViolation');
     }
 
     /**
@@ -34,6 +128,7 @@ class BothPropertiesPresentValidatorTest extends TestCase
      */
     public function it_passes_validation_if_both_values_are_not_null(): void
     {
+        $this->markTestIncomplete('to do');
     }
 
     /**
@@ -41,5 +136,49 @@ class BothPropertiesPresentValidatorTest extends TestCase
      */
     public function it_fails_validation_if_only_one_value_is_defined(): void
     {
+        $valueToValidate = new \stdClass();
+        $property1 = 'someProperty';
+        $property2 = 'someOtherProperty';
+
+        $this->propertyAccessor->expects('isReadable')
+            ->with($valueToValidate, $property1)
+            ->andReturn(true);
+
+        $this->propertyAccessor->expects('isReadable')
+            ->with($valueToValidate, $property2)
+            ->andReturn(true);
+
+        $this->propertyAccessor->expects('getValue')
+            ->with($valueToValidate, $property1)
+            ->andReturn(42);
+
+        $this->propertyAccessor->expects('getValue')
+            ->with($valueToValidate, $property2)
+            ->andReturn(null);
+
+        $message = 'validation error';
+
+        $this->context->expects('buildViolation')
+            ->with($message)
+            ->andReturn($builder = \Mockery::mock(ConstraintViolationBuilderInterface::class));
+
+        $builder->expects('setParameter')
+            ->with('%property1%', $property1)
+            ->andReturnSelf();
+
+        $builder->expects('setParameter')
+            ->with('%property2%', $property2)
+            ->andReturnSelf();
+
+        $builder->expects('addViolation');
+
+        $this->SUT->validate(
+            $valueToValidate,
+            new BothPropertiesPresent(
+                property1: $property1,
+                property2: $property2,
+                message: $message,
+            )
+        );
     }
 }
